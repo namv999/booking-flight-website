3c2630d7a4c9059cafd72e5ca873ea8ec52852ef
NamVo <namv2907@gmail.com>
2026-08-26 15:42:27 +0700

    Cập nhật, bổ sung Seeders cho database/seeders/DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,15 +7,24 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
-    {
-        $this->call([
-            UserSeeder::class,
-            AirlineSeeder::class,
-            AirportSeeder::class,
-            BaggageAddonSeeder::class,
-            FareClassSeeder::class,
-            AircraftSeeder::class,
-            SeatSeeder::class,
-        ]);
-    }
+{
+    $this->call([
+        UserSeeder::class,
+        AirlineSeeder::class,
+        AirportSeeder::class,
+        BaggageAddonSeeder::class,
+        FareClassSeeder::class,
+        AircraftSeeder::class,
+        SeatSeeder::class,
+        FlightSeeder::class,
+        FlightSeatSeeder::class,
+        BookingSeeder::class,
+        BookingFlightSeeder::class,
+        PassengerSeeder::class,
+        SavedPassengerSeeder::class,
+        TicketSeeder::class,
+        BookingTotalSeeder::class,
+        PaymentSeeder::class,
+    ]);
+}
 }
